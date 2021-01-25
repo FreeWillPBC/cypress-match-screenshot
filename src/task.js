c3e0f70c24e58373bbbb0e6300ce8e95c979d2b7
Alden Quimby <alden@freewill.com>
2021-01-25 14:16:28 -0500

    Include full describe heirarchy in file name

--- src/task.js
+++ src/task.js
@@ -1,40 +1,42 @@
+/* eslint-disable no-unused-vars */
 const fs = require('fs');
+
 const touch = require('touch');
 
 function registerTask(on, config) {
   on('task', {
     mkdir(newDir) {
-      return new Promise(function(resolve, reject) {
+      return new Promise((resolve, reject) => {
         fs.mkdir(newDir, resolve);
       });
     },
     touch(path) {
-      return new Promise(function(resolve, reject) {
+      return new Promise((resolve, reject) => {
         touch(path, null, resolve);
       });
     },
     rename({ from, to }) {
-      return new Promise(function(resolve, reject) {
+      return new Promise((resolve, reject) => {
         fs.rename(from, to, resolve);
       });
     },
     copy({ from, to }) {
-      return new Promise(function (resolve, reject) {
+      return new Promise((resolve, reject) => {
         fs.createReadStream(from).pipe(fs.createWriteStream(to)).on('finish', () => {
           resolve(null);
         });
       });
     },
     unlink(path) {
-      return new Promise(function(resolve, reject) {
+      return new Promise((resolve, reject) => {
         fs.unlink(path, resolve);
       });
     },
     exists(path) {
-      return new Promise(function (resolve, reject) {
+      return new Promise((resolve, reject) => {
         fs.exists(path, resolve);
       });
-    }
+    },
   });
 }
 
